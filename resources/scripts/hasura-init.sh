5890403741f713d15598cf98fe4b502b2988a910
naveen <naveen@metalimits.com>
2023-07-19 16:27:40 +0530

    updated messages

--- resources/scripts/hasura-init.sh
+++ resources/scripts/hasura-init.sh
@@ -20,11 +20,11 @@ HASURA_ENDPOINT=${GRAPHQL_URL}  # your Hasura endpoint
 DATABASE_URL="postgres://$fabriq_db_user:$fabriq_db_pass@$fabriq_db_host:$fabriq_db_port/$fabriq_db_name" 
 METADATA_FILE="../../resources/hasura_metadata.json"  # Path to your hasura_metadata.json file
 
-echo "Append database details in the metadata Inprogress"
+echo "Append database details in the metadata is inprogres"
 # Update metadata.json with database_url
 sed -i "s|\"database_url\": \".*\"|\"database_url\": \"$DATABASE_URL\"|" $METADATA_FILE
 
-echo "Append database details in the metadata Successfull"
+echo "Append database details in the metadata Successfully"
 
 echo "Import Metadata to hasura $HASURA_ENDPOINT"
 # cat $METADATA_FILE
@@ -33,4 +33,4 @@ curl -X POST "$HASURA_ENDPOINT/v1/metadata" \
      -H "X-Hasura-Admin-Secret: $hasura_admin_secret" \
      -d '{"type":"replace_metadata", "args":'"$(cat $METADATA_FILE)"'}'
 
-echo "Import Metadata Successfull"
+echo "Import Metadata Successfully"
