99428570b3234bd6601b55effbac78875cc288e5
MadhanKumarR <madhan@metalimits.com>
2023-08-24 20:18:06 +0530

    Added custom variables for scroll depth.

--- transforms/dbt-fabriq-web/models/redshift/article/articles_monthly.sql
+++ transforms/dbt-fabriq-web/models/redshift/article/articles_monthly.sql
@@ -96,7 +96,7 @@ article_monthly AS (
         COUNT(DISTINCT page_view_id) AS page_views,
         COUNT(DISTINCT CASE WHEN domain_sessionidx = 1 THEN cba.domain_sessionid ELSE NULL END) AS new_users,
         SUM(case when page_views_in_session = 1 then 1 else 0 end)::decimal / COUNT(distinct cba.domain_sessionid)::decimal as bounce_rate,
-        SUM(CASE WHEN vertical_percentage_scrolled >= 100 THEN 1 ELSE 0 END)::decimal / COUNT(DISTINCT domain_userid)::decimal AS readability,
+        SUM(CASE WHEN vertical_percentage_scrolled >= COALESCE({{var('snowplow_web')['snowplow__readability_percentage']}}, 100) THEN 1 ELSE 0 END)::decimal / COUNT(DISTINCT domain_userid)::decimal AS readability,
         COUNT(DISTINCT cba.domain_sessionid)::DECIMAL / COUNT(distinct domain_userid)::DECIMAL as session_per_user,
         COUNT(DISTINCT domain_userid) AS users,
         SUM(engaged_time_in_s) AS total_time_spent,
